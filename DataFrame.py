594aa8028023e571d5528991faf2674bd2f9e727
muhammadaahsan1 <96174697+muhammadaahsan1@users.noreply.github.com>
2022-05-15 10:03:35 +0500

    Create DataFrame.py

---
+++ DataFrame.py
@@ -0,0 +1,10 @@
+
+
+import pandas as pd
+L1=[1,2,3,4,5]
+df1=pd.DataFrame(L1)
+print(df1)
+
+D1={'Fruit names':['apple', 'mango', 'banana'], 'count': ['5', '10', '20']}
+df2=pd.DataFrame(D1)
+print(df2)
